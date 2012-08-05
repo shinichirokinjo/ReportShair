f7576bf414775ae105c3c99c9c1400a8262da187
tokuda109 <tokuda109@gmail.com>
2012-08-05 16:07:19 +0900

    Updated config

--- app/Config/Schema/reports.sql
+++ app/Config/Schema/reports.sql
@@ -1,15 +1,17 @@
 CREATE TABLE IF NOT EXISTS `reports` (
   `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
   `title` varchar(100) NOT NULL,
   `body` text DEFAULT NULL,
   `date` datetime DEFAULT NULL,
   `place` varchar(100) DEFAULT NULL,
+  `status` enum('delete', 'publish') NOT NULL DEFAULT 'publish',
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=1 ;
 
 INSERT INTO `reports` VALUES 
-(1, 'RedTrash', '', '2012-10-10 00:00:01', '', now(), now()),
-(2, 'Bigbeach fes 2012', '', '2012-10-11 00:00:01', '', now(), now()),
-(3, 'Metamorphose2012', '', '2012-10-12 00:00:01', '', now(), now());
+(1, 1, 'RedTrash', '', '2012-10-10 00:00:01', '', 'publish', now(), now()),
+(2, 1, 'Bigbeach fes 2012', '', '2012-10-11 00:00:01', '', 'publish', now(), now()),
+(3, 1, 'Metamorphose2012', '', '2012-10-12 00:00:01', '', 'publish', now(), now());
--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,17 +1,21 @@
 <?php
 
+	// Home
 	Router::connect('/', array('controller' => 'home', 'action' => 'index', 'home'));
 
+	// Pages
 	Router::connect('/about/', array('controller' => 'pages', 'action' => 'about'));
 	Router::connect('/terms', array('controller' => 'pages', 'action' => 'terms'));
 	Router::connect('/policy', array('controller' => 'pages', 'action' => 'policy'));
 
+	// Reports
 	Router::connect('/reports/', array('controller' => 'reports', 'action' => 'index'));
 	Router::connect('/reports/add', array('controller' => 'reports', 'action' => 'add'));
 	Router::connect('/reports/edit/*', array('controller' => 'reports', 'action' => 'edit'));
 	Router::connect('/reports/delete/*', array('controller' => 'reports', 'action' => 'delete'));
 	Router::connect('/reports/*', array('controller' => 'reports', 'action' => 'view'));
 
+	// Users
 	Router::connect('/users/', array('controller' => 'users', 'action' => 'index'));
 	Router::connect('/users/callback', array('controller' => 'users', 'action' => 'callback'));
 	Router::connect('/users/regist', array('controller' => 'users', 'action' => 'regist'));
