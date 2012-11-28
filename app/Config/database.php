f489dbc012940849695d2da5c1e70f13b8352a9d
root <root@localhost.localdomain>
2012-11-27 23:07:21 -0500

    stages

---
+++ app/Config/database.php
@@ -0,0 +1,5 @@
+<?php
+(isset($_SERVER['CAKE_ENV']) || isset($_ENV['CAKE_ENV'])) or die('stages is not found.');
+if(isset($_SERVER['CAKE_ENV'])) $env = $_SERVER['CAKE_ENV'];
+if(isset($_ENV['CAKE_ENV'])) $env = $_ENV['CAKE_ENV'];
+return require_once( dirname(__FILE__) . DS . "Stages" . DS . $env . DS . "database.php");
